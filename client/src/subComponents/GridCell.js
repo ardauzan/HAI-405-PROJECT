675b61aaf3a3b46835bbfbf49ee4a8322b0bce8a
arda.uzan <arda.uzan@protonmail.com>
2022-04-25 00:30:17 +0200

    beta2

--- client/src/subComponents/GridCell.js
+++ client/src/subComponents/GridCell.js
@@ -3,10 +3,9 @@ import PropTypes from "prop-types"
 import { ErrorBoundary } from "../components"
 import { allQuestionsState } from "../state"
 import { useRecoilValue } from "recoil"
-import { _parseAllQuestions } from "../utils"
+import { _parseQuestions } from "../utils"
 import eliminated from "../cross.png"
 import styles from "./GridCell.module.sass"
-import { _parseQuestions } from "../utils"
 const { cross, defimg, container, testgrid } = styles
 export default function GridCell({ vi: [v, i] }) {
 	const [imgAlt, imgSrc] = ["image-" + (i + 1), "images/image-" + (i + 1) + ".png"]
@@ -15,11 +14,10 @@ export default function GridCell({ vi: [v, i] }) {
 		<ErrorBoundary level='cell'>
 			<article className={container}>
 				<div className={testgrid}>
-					{_parseAllQuestions(v, allQuestions) ? (
-						<div className={cross}>
-							<img alt='eliminated' src={eliminated} />
-						</div>
-					) : null}
+					<div className={cross}>
+						<img alt='eliminated' src={eliminated} />
+					</div>
+
 					<div className={defimg}>
 						<img alt={imgAlt} src={imgSrc} />
 					</div>
